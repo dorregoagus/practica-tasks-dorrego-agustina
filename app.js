c6e37f6c4eb68b8e15e91900be3f870ef3c28f0b
dorregoagus <dorregoagus18@gmail.com>
2026-08-31 11:50:42 -0300

    Se crearon los modelos user y task

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import sequelize from "./src/config/database.js";
+import User from "./src/models/user.js";
+import Task from "./src/models/task.js";
 
 try {
     await sequelize.authenticate();
     console.log("Se estableció la conección con la base de datos.");
+
+    await sequelize.sync();
+    console.log("Tablas creadas correctamente.");
 } catch (error) {
     console.error("No se pudo establecer la conección con la base de datos:", error);
 };
